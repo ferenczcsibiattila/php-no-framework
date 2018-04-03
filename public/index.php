7588b6e43f5554aa31243f7ebf3418df840ecce8
Ferencz-Csibi Attila <ferencz.csibi.Attila@ujvilag.gov.hu>
2018-04-03 15:01:26 +0200

    add middleware dispatcher, psr-7 implementation, fastRoute request handler
    connect with di container

--- public/index.php
+++ public/index.php
@@ -1,29 +1,60 @@
 <?php
+// declare(encoding='UTF-8'); Zend multibyte feature is turned off
 declare(strict_types=1);
 
 require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
 // echo 'Hello, world!';
-$helloWorld = new \ExampleApp\HelloWorld;
-echo $helloWorld->announce();
-echo '<br />';
+// $helloWorld = new \ExampleApp\HelloWorld;
+// echo $helloWorld->announce();
+// echo '<br />';
 
-use \DI\ContainerBuilder;
-use \ExampleApp\HelloWorld;
-use function DI\create; // functions.php
+	use \DI\ContainerBuilder;
+	use \ExampleApp\HelloWorld;
+
+	// FastRoute
+	use \FastRoute\RouteCollector;
+	use \Middlewares\FastRoute;
+	use \Middlewares\RequestHandler;
+
+	// Let's get Relay ready to accept middleware.
+	use \Relay\Relay;
+	use \Zend\Diactoros\ServerRequestFactory;
+
+	use function \DI\create; // functions.php
+	use function \DI\get; // functions.php
+
+	// FastRoute
+	use function \FastRoute\simpleDispatcher;
 
 // Dependency injection container
 //
-$containerBuilder = new \DI\ContainerBuilder;
-$containerBuilder->useAutowiring(FALSE);
-$containerBuilder->useAnnotations(FALSE);
+	$containerBuilder = new \DI\ContainerBuilder;
+	$containerBuilder->useAutowiring(FALSE);
+	$containerBuilder->useAnnotations(FALSE);
 
-$containerBuilder->addDefinitions(array(
-		\ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)
-	)
-);
+	$containerBuilder->addDefinitions(array(
+			\ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)->constructor(\DI\get('Foo')),
+			'Foo'                         => 'bar'
+		)
+	);
 
-$container = $containerBuilder->build();
+	$container = $containerBuilder->build();
+
+// FastRoute - determines if a request is valid and can actually be handled by the application
+//
+	$routes = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+	    $r->get('/hello', HelloWorld::class);
+	});
+
+	$middlewareQueue[] = new \Middlewares\FastRoute($routes);
+	$middlewareQueue[] = new \Middlewares\RequestHandler($container);
+
+// Diactoros - Let's get Relay ready to accept middleware.
+	$requestHandler = new \Relay\Relay($middlewareQueue);
+	// the request handler sends Request to the handler configured for that route in the routes definition
+	$requestHandler->handle(\Zend\Diactoros\ServerRequestFactory::fromGlobals());
 
 $helloWorld = $container->get(\ExampleApp\HelloWorld::class);
 echo $helloWorld->announce();
+
--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -5,8 +5,21 @@ namespace ExampleApp;
 
 class HelloWorld
 {
+    private $foo;
+
+    public function __construct(string $foo)
+    {
+        $this->foo = $foo;
+    }
+
+    public function __invoke(): void
+    {
+        echo "Hello, {$this->foo} in an invoked world!";
+        exit;
+    }
+
 	public function announce(): string
 	{
-		return 'Hello autoloaded world!';
+		return "Hello, {$this->foo} in a front controller world!";
 	}
 }
