03973957b790d70c731fded9d2c0006750fd56ec
Ferencz-Csibi Attila <ferencz.csibi.Attila@ujvilag.gov.hu>
2018-04-03 11:20:34 +0200

    DI container

--- public/index.php
+++ public/index.php
@@ -6,3 +6,18 @@ require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'.DIREC
 // echo 'Hello, world!';
 $helloWorld = new \ExampleApp\HelloWorld;
 echo $helloWorld->announce();
+
+// Dependency injection container
+//
+$containerBuilder = new \DI\ContainerBuilder;
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions(array(
+		\ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)
+	)
+);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(\ExampleApp\HelloWorld::class);
+echo $helloWorld->announce();
