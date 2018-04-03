0fc9963b16aa02d66aa75c716260cb8bdde076f1
Ferencz-Csibi Attila <ferencz.csibi.Attila@ujvilag.gov.hu>
2018-04-03 17:46:46 +0200

    format

--- public/index.php
+++ public/index.php
@@ -1,70 +1,61 @@
 <?php
-// declare(encoding='UTF-8'); Zend multibyte feature is turned off
 declare(strict_types=1);
 
 require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-// echo 'Hello, world!';
-// $helloWorld = new \ExampleApp\HelloWorld;
-// echo $helloWorld->announce();
-// echo '<br />';
+use \DI\ContainerBuilder;
+use \ExampleApp\HelloWorld;
 
-	use \DI\ContainerBuilder;
-	use \ExampleApp\HelloWorld;
+// FastRoute
+use \FastRoute\RouteCollector;
+use \Middlewares\FastRoute;
+use \Middlewares\RequestHandler;
 
-	// FastRoute
-	use \FastRoute\RouteCollector;
-	use \Middlewares\FastRoute;
-	use \Middlewares\RequestHandler;
+use \Relay\Relay;
 
-	// Let's get Relay ready to accept middleware.
-	use \Relay\Relay;
-	use \Zend\Diactoros\ServerRequestFactory;
+use \Zend\Diactoros\ServerRequestFactory;
+use \Zend\Diactoros\Response;
+use \Zend\Diactoros\Response\SapiEmitter;
 
-	use \Zend\Diactoros\Response;
-	use \Zend\Diactoros\Response\SapiEmitter;
+use function \DI\create; // functions.php
+use function \DI\get; // functions.php
 
-	use function \DI\create; // functions.php
-	use function \DI\get; // functions.php
+use function \FastRoute\simpleDispatcher;
 
-	// FastRoute
-	use function \FastRoute\simpleDispatcher;
+$containerBuilder = new \DI\ContainerBuilder; // Dependency injection container
+$containerBuilder->useAutowiring(FALSE);
+$containerBuilder->useAnnotations(FALSE);
 
-// Dependency injection container
-//
-	$containerBuilder = new \DI\ContainerBuilder;
-	$containerBuilder->useAutowiring(FALSE);
-	$containerBuilder->useAnnotations(FALSE);
+// class definitions for DI
+$containerBuilder->addDefinitions(array(
+		\ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)->constructor(\DI\get('Foo'), \DI\get('Response')),
+		'Foo'                         => 'bar',
+		'Response'                    => function() {
+			return new \Zend\Diactoros\Response();
+		},
+	)
+);
 
-	$containerBuilder->addDefinitions(array(
-			\ExampleApp\HelloWorld::class => \DI\create(\ExampleApp\HelloWorld::class)->constructor(\DI\get('Foo'), \DI\get('Response')),
-			'Foo'                         => 'bar',
-			'Response'                    => function() {
-				return new \Zend\Diactoros\Response();
-    		},
-		)
-	);
-
-	$container = $containerBuilder->build();
+$container = $containerBuilder->build();
 
 // FastRoute - determines if a request is valid and can actually be handled by the application
-//
-	$routes = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
-	    $r->get('/hello', HelloWorld::class);
-	});
+$routes = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
 
-	$middlewareQueue[] = new \Middlewares\FastRoute($routes);
-	$middlewareQueue[] = new \Middlewares\RequestHandler($container);
+$middlewareQueue[] = new \Middlewares\FastRoute($routes);
+$middlewareQueue[] = new \Middlewares\RequestHandler($container);
 
 // Diactoros - Let's get Relay ready to accept middleware.
-	$requestHandler = new \Relay\Relay($middlewareQueue);
-	// the request handler sends Request to the handler configured for that route in the routes definition
-	$response = $requestHandler->handle(\Zend\Diactoros\ServerRequestFactory::fromGlobals());
+$requestHandler = new \Relay\Relay($middlewareQueue);
+
+// the request handler sends Request to the handler configured for that route in the routes definition
+$response = $requestHandler->handle(\Zend\Diactoros\ServerRequestFactory::fromGlobals());
 
 // Emitter Response to SAPI
-	$emitter = new \Zend\Diactoros\Response\SapiEmitter();
-	return $emitter->emit($response);
+$emitter = new \Zend\Diactoros\Response\SapiEmitter();
+return $emitter->emit($response);
 
+// Non-routed non-emitter result
 $helloWorld = $container->get(\ExampleApp\HelloWorld::class);
 echo $helloWorld->announce();
-
